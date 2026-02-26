ed0bac06645f38261a441ab76f9ffc7f29c524ae
Korneva Anastasia <ivan@mail.ru>
2026-02-26 15:43:18 +0700

    Сделала файл с исходными данными

---
+++ data shield operation.py
@@ -0,0 +1,45 @@
+# operation_data_shield.py
+import re
+import base64
+import codecs
+with open('text.txt', 'r', encoding='utf-8') as f:
+    main_text = f.read()
+def decode_messages(text):
+    """
+    finds and decrypts messages
+    :param text: text to decode
+    :return: {'base64': [], 'hex': [], 'rot13': []}
+    """
+    result = {
+        'base64': [],
+        'hex': [],
+        'rot13': []
+    }
+    #base64 codes
+    b64_codes = re.findall(r'Base64:\s*([A-Za-z0-9+/=]+)', text)
+    for b64 in b64_codes:
+        b64_bytes = base64.b64decode(b64)
+        b64_str =b64_bytes.decode('utf-8')
+        result['base64'].append(f'{b64} -> {b64_str}')
+
+    #hex 0x.. codes
+    hex_type1 = re.findall(r'Hex:\s*(0x[0-9A-Fa-f]+)', text)
+    #hex \xHH.. codes
+    hex_type2 = re.findall(r'(?:\\x[0-9A-Fa-f]{2})+', text)
+    for hex_code in hex_type1:
+        clean_hex = hex_code.replace('0x', '')
+        hex_bytes = bytes.fromhex(clean_hex)
+        hex_str = hex_bytes.decode('utf-8')
+        result['hex'].append(f'{hex_code} -> {hex_str}')
+    for hex_code in hex_type2:
+        clean_hex = hex_code.replace('\\x', '')
+        hex_bytes = bytes.fromhex(clean_hex)
+        hex_str = hex_bytes.decode('utf-8')
+        result['hex'].append(f'{hex_code} -> {hex_str}')
+
+    #rot13
+    rot_codes = re.findall(r'ROT13:\s*(.+)', text)
+    for rot in rot_codes:
+        rot_str = codecs.encode(rot, 'rot13')
+        result['rot13'].append(f'{rot} -> {rot_str}')
+    return result

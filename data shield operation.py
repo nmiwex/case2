e3c3c7bfac15bd6fe2ac38675e665ff95fd2ff1f
Veronika Timofeeva <veronika.t.2801@mail.ru>
2026-02-26 18:40:25 +0700

    exceptions for decode messages

--- data shield operation.py
+++ data shield operation.py
@@ -18,28 +18,37 @@ def decode_messages(text):
     #base64 codes
     b64_codes = re.findall(r'Base64:\s*([A-Za-z0-9+/=]+)', text)
     for b64 in b64_codes:
-        b64_bytes = base64.b64decode(b64)
-        b64_str =b64_bytes.decode('utf-8')
-        result['base64'].append(f'{b64} -> {b64_str}')
+        try:
+            b64_bytes = base64.b64decode(b64)
+            b64_str =b64_bytes.decode('utf-8')
+            result['base64'].append(f'{b64} -> {b64_str}')
+        except UnicodeDecodeError:
+            result['base64'].append(f'{b64} -> Ошибка декодирования!')
 
     #hex 0x.. codes
     hex_type1 = re.findall(r'Hex:\s*(0x[0-9A-Fa-f]+)', text)
     #hex \xHH.. codes
     hex_type2 = re.findall(r'(?:\\x[0-9A-Fa-f]{2})+', text)
     for hex_code in hex_type1:
-        clean_hex = hex_code.replace('0x', '')
-        hex_bytes = bytes.fromhex(clean_hex)
-        hex_str = hex_bytes.decode('utf-8')
-        result['hex'].append(f'{hex_code} -> {hex_str}')
+        try:
+            clean_hex = hex_code.replace('0x', '')
+            hex_bytes = bytes.fromhex(clean_hex)
+            hex_str = hex_bytes.decode('utf-8')
+            result['hex'].append(f'{hex_code} -> {hex_str}')
+        except UnicodeDecodeError:
+            result['hex'].append(f'{hex_code} -> Ошибка декодирования!')
     for hex_code in hex_type2:
-        clean_hex = hex_code.replace('\\x', '')
-        hex_bytes = bytes.fromhex(clean_hex)
-        hex_str = hex_bytes.decode('utf-8')
-        result['hex'].append(f'{hex_code} -> {hex_str}')
+        try:
+            clean_hex = hex_code.replace('\\x', '')
+            hex_bytes = bytes.fromhex(clean_hex)
+            hex_str = hex_bytes.decode('utf-8')
+            result['hex'].append(f'{hex_code} -> {hex_str}')
+        except UnicodeDecodeError:
+            result['hex'].append(f'{hex_code} -> Ошибка декодирования!')
 
     #rot13
-    rot_codes = re.findall(r'ROT13:\s*(.+)', text)
+    rot_codes = re.findall(r'ROT13:\s*([A-Za-z0-9\s.,!?;:\'"\-\(\)]*)', text)
     for rot in rot_codes:
-        rot_str = codecs.encode(rot, 'rot13')
+        rot_str = codecs.decode(rot, 'rot13')
         result['rot13'].append(f'{rot} -> {rot_str}')
     return result

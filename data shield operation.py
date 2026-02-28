3a2eb5f58b772e833fe8e7dac506b9a2bce14aec
Veronika Timofeeva <veronika.t.2801@mail.ru>
2026-02-28 20:25:29 +0700

    normalize dates, inn, phones

--- data shield operation.py
+++ data shield operation.py
@@ -3,19 +3,20 @@ import re
 import base64
 import codecs
 import binascii
+from datetime import datetime
 with open('data_leak_sample.txt', 'r', encoding='utf-8') as f:
     main_text = f.read()
 
 
-def add_unique(to_add, list):
+def add_unique(to_add, lst):
     """
     adds something to the list if it is unique
     :param to_add: something to be added
-    :param list: some list
+    :param lst: some list
     :return:None
     """
-    if to_add not in list:
-        list.append(to_add)
+    if to_add not in lst:
+        lst.append(to_add)
 
 
 def decode_messages(text):
@@ -53,9 +54,11 @@ def decode_messages(text):
             hex_str = hex_bytes.decode('utf-8')
             result['hex'].append(f'{hex_code} -> {hex_str}')
         except ValueError:
-            result['hex'].append(f'{hex_code} -> Ошибка: нечетное количество символов или недопустимые знаки"')
+            result['hex'].append(f'{hex_code} -> Ошибка: нечетное количество'
+                                 f' символов или недопустимые знаки')
         except UnicodeDecodeError:
-            result['hex'].append(f'{hex_code} -> Ошибка декодирования: Содержит нечитаемые бинарные данные')
+            result['hex'].append(f'{hex_code} -> Ошибка декодирования: '
+                                 f'Содержит нечитаемые бинарные данные')
 
     #rot13
     for _ in re.finditer(r'ROT13:\s*([A-Za-z0-9 .,!?;:\'"\-\(\)]*)', text):
@@ -63,7 +66,7 @@ def decode_messages(text):
         rot_str = codecs.decode(rot, 'rot13')
         result['rot13'].append(f'{rot} -> {rot_str}')
     return result
-print(decode_messages(main_text))
+
 
 def normalize_and_validate(text):
     """
@@ -91,7 +94,8 @@ def normalize_and_validate(text):
         elif len(digits) == 11 and digits[0] in '78':
             if digits[0] == '8':
                 digits = '7' + digits[1:]
-            normalized_phone = f'+{digits[0]} ({digits[1:4]}) {digits[4:7]}-{digits[7:9]}-{digits[9:]}'
+            normalized_phone = (f'+{digits[0]} ({digits[1:4]}) '
+                                f'{digits[4:7]}-{digits[7:9]}-{digits[9:]}')
             add_unique(normalized_phone, result['phones']['valid'])
         else:
             add_unique(phone, result['phones']['invalid'])
@@ -100,7 +104,91 @@ def normalize_and_validate(text):
         digits = re.sub(r'\D', '', phone)
         if len(digits) != 10:
             continue
-        normalized_phone = f'+7 ({digits[:3]}) {digits[3:6]}-{digits[6:8]}-{digits[8:]}'
+        normalized_phone = (f'+7 ({digits[:3]}) '
+                            f'{digits[3:6]}-{digits[6:8]}-{digits[8:]}')
         add_unique(normalized_phone, result['phones']['valid'])
+
+    #inn
+    coef10 = [2, 4, 10, 3, 5, 9, 4, 6, 8]
+    coef11 = [7, 2, 4, 10, 3, 5, 9, 4, 6, 8]
+    coef12 = [3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8]
+    for _ in re.finditer(r'\b(\d{10,13}[ \-]?)', text):
+        inn = _.group(0)
+        digits = re.sub(r'\D', '', inn)
+        digits_int = list(map(int, digits))
+        if len(digits) == 10:
+            ctrl_sum = sum(digits_int[i] * coef10[i] for i in range(9))
+            ctrl_digit = (ctrl_sum % 11) % 10
+            if ctrl_digit == digits_int[-1]:
+                add_unique(inn, result['inn']['valid'])
+            else:
+                add_unique(inn, result['inn']['invalid'])
+        elif len(digits) == 12:
+            ctrl_sum1 = sum(digits_int[i] * coef11[i] for i in range(10))
+            ctrl_digit1 = (ctrl_sum1 % 11) % 10
+            ctrl_sum2 = sum(digits_int[i] * coef12[i] for i in range(11))
+            ctrl_digit2 = (ctrl_sum2 % 11) % 10
+            if ctrl_digit1 == digits_int[-2] and ctrl_digit2 == digits_int[-1]:
+                add_unique(digits, result['inn']['valid'])
+            else:
+                add_unique(digits, result['inn']['invalid'])
+
+    #dates
+    def parse_date(d):
+        for fmt in formats:
+            try:
+                return datetime.strptime(d, fmt)
+            except ValueError:
+                continue
+        m = re.match(r"^\s*(\d{1,2})\s*([А-Яа-я]+)\s*(\d{4})\s*$", d)
+        if m:
+            day = int(m.group(1))
+            mon = m.group(2).lower()
+            year = int(m.group(3))
+
+            if mon in ru_month:
+                month = ru_month[mon]
+            else:
+                mon3 = mon[:3]
+                if mon3 in ru_month:
+                    month = ru_month[mon3]
+                else:
+                    return None
+
+            try:
+                return datetime(year, month, day).date()
+            except Exception:
+                return None
+        return None
+    formats = [
+        '%d.%m.%Y', '%d/%m/%Y', '%d-%m-%Y', '%Y.%m.%d', '%Y-%m-%d', '%Y/%m/%d',
+        '%m.%d.%Y', '%m/%d/%Y', '%m-%d-%Y', '%d %B %Y', '%d-%B-%Y', '%d %b %Y', '%d-%b-%Y'
+    ]
+    ru_month = {
+        "янв": 1, "января": 1, "январь": 1,
+        "фев": 2, "февраля": 2, "февраль": 2,
+        "мар": 3, "марта": 3, "март": 3,
+        "апр": 4, "апреля": 4, "апрель": 4,
+        "май": 5, "мая": 5,
+        "июн": 6, "июня": 6, "июнь": 6,
+        "июл": 7, "июля": 7, "июль": 7,
+        "авг": 8, "августа": 8, "август": 8,
+        "сен": 9, "сент": 9, "сентября": 9, "сентябрь": 9,
+        "окт": 10, "октября": 10, "октябрь": 10,
+        "ноя": 11, "ноября": 11, "ноябрь": 11,
+        "дек": 12, "декабря": 12, "декабрь": 12,
+    }
+    for _ in re.finditer(r'\b\d{1,4}[^0][./-]\d{1,2}[./-]\d{2,4}\b|'
+                         r'\b\d{1,2}[ \-][A-Za-zа-яА-Я]{3,}[ \-]\d{4}\b', text):
+        date = _.group(0).strip()
+        try:
+            dt = parse_date(date)
+            if dt is None:
+                add_unique(date, result['dates']['invalid'])
+            else:
+                add_unique(dt.strftime('%d.%m.%Y'), result['dates']['normalized'])
+        except ValueError:
+            add_unique(date, result['dates']['invalid'])
+
     return result
 print(normalize_and_validate(main_text))
